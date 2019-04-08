ef93e11f2ba721f8980cabb81a33a5417c7b07ad
reidmalone <reidmalone@gmail.com>
2019-04-08 14:11:17 -0500

    formatted data & added tooltip

--- wordle.js
+++ wordle.js
@@ -2,14 +2,8 @@ var width = 750;
 var height = 500;
 var fontScale = d3.scale.linear().range([10, 60]);
 var fill = d3.scale.category20();
-/*
-// Initialize slider
-var slider1 = d3.slider().min(0).max(10).ticks(10).showRange(true).value(4);
-var slider2 = d3.slider().min(0).max(10).ticks(10).showRange(true).value(6);
-// Render the slider in the div
-d3.select('#slider1').call(slider1);
-d3.select('#slider2').call(slider2);
-*/
+
+
 var svg = d3.select("#vis").append("svg")
     .attr("width", width)
     .attr("height", height)
@@ -34,17 +28,28 @@ d3.csv("ufo.csv", function(error, data) {
     //nested = d3.nest().key(d=>d.Shape).entries(data);
     //counting = nested.map(d=>{d.count=d.values.length; return d});
     //selectedData = data.filter(d=>d.Time < "10/10/1971 23:4" && d.Time>"10/10/1949 20:30"); //this is for the slider crap
-
+    let div = d3.select("body").append("div").attr("class", "tooltip").attr("opacity", 0);
     var dropDown = d3.select("#drop")
         .append("select")
         .attr("name", "dates");
-
+    /*
+    var dropDown = d3.select("#drop")
+        .append("select")
+        .attr("name", "dates");
+*/
     var newData = data.map(function(d) {
         let newItem = {};
         newItem.Time = d.Time;
         newItem.Shape = d.Shape;
         return newItem;
     })
+
+//formats the date here
+    newData.forEach(function(d,i){
+        d3.time.format("%Y-%m-%d").parse(d.Time);
+        return d;
+    });
+    console.log(newData.Time);
 
     selectedData = newData.filter(d=>d.Time < "10/10/2009 23:40" && d.Time>"10/10/1949 20:30"); //this is for the slider crap
     nested = d3.nest().key(d=>d.Shape).entries(selectedData);
@@ -60,16 +65,32 @@ d3.csv("ufo.csv", function(error, data) {
         .attr("value", function(d) {
             return d.value;
         })
+
+
 */
+/*
+    var options2 = dropDown2.selectAll("option")
+        .data(selectedData)
+        .enter()
+        .append("option")
+        .text(function(d) {
+            return d.Time;
+        })
+    dropDown1.on("change", update(options1.value,options2.value));
+
+ */
     dropDown.on("change", update);
 
     function update() {
 
+
+        //selectedData = newData.filter(d=>d.Time < min && d.Time>max); //this is for the slider crap
+
         selectedData = newData.filter(d=>d.Time < "10/10/2009 23:40" && d.Time>this.value); //this is for the slider crap
         nested = d3.nest().key(d=>d.Shape).entries(selectedData);
         counting = nested.map(d=>{d.count=d.values.length; return d});
-        //console.log(selectedData);
-        console.log(counting);
+
+        //console.log(counting);
         fontScale.domain([
             d3.min(counting, function(d) {
                 return d.count
@@ -155,5 +176,25 @@ d3.csv("ufo.csv", function(error, data) {
             .style('fill-opacity', 1e-6)
             .attr('font-size', 1)
             .remove();
+
+
+        selectVis.on("mouseover", d => {
+            div.style('left', d3.event.pageX + "px").style("top", d3.event.pageY + "px");
+            div.transition().duration(1000).style("opacity", 1);
+            div.html(d.count);
+
+            paused = true;
+        })
+            .on("mouseout", d => {
+                div.transition().duration(1000).style("opacity", 0);
+                paused = false;
+            });
+        //endnew
+
     }
+
+//need help here too :)
+
+
+
 });
